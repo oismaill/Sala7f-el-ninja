a5d6212a2506cb5d672796d0b6a9fa8c30588df6
Nada Ashraf ElRefaai <32249121+NadaElRefaai@users.noreply.github.com>
2017-09-29 23:08:56 +0200

    added hotel.java
    this is wheree we'll work

---
+++ Hotel/src/hotel/Hotel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel;
+import java.util.*;
+
+public class Hotel {
+    
+    class Employee
+    {
+        int ID;
+        String Name;
+        String Address;
+        String PhoneNumber;
+        int Salary;
+        
+        
+    }
+    
+    class Payment
+    {
+        
+    }
+            
+   
+    public static void main(String[] args) {
+    }
+    
+}
